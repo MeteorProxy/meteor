aa16aed352e75c8920774c58938b410a4eca2a14
proudparrot2 <knowing.discos0e@icloud.com>
2024-07-08 15:30:32 -0500

    add esbuild to dev server

--- server.ts
+++ server.ts
@@ -2,11 +2,14 @@ import fastifyStatic from '@fastify/static'
 import { baremuxPath } from '@mercuryworkshop/bare-mux/node'
 import Fastify from 'fastify'
 import wisp from 'wisp-server-node'
+import fs from 'node:fs'
 
 import { createServer } from 'node:http'
 import type { Socket } from 'node:net'
 import { fileURLToPath } from 'node:url'
 import { consola } from 'consola'
+import { resolve } from 'node:path'
+import { context } from 'esbuild'
 
 const port = Number(process.env.PORT) || 9000
 
@@ -31,6 +34,23 @@ Fastify({
     prefix: '/bare-mux/',
     decorateReply: false
   })
+
   .listen({ port }, () => {
     consola.success(`Server listening on http://localhost:${port}`)
   })
+
+const dev = await context({
+  sourcemap: true,
+  minify: process.env.NODE_ENV !== 'development',
+  entryPoints: {
+    'meteor.bundle': './src/bundle/index.ts',
+    'meteor.client': './src/client/index.ts',
+    'meteor.worker': './src/worker.ts',
+    'meteor.config': './src/config.ts'
+  },
+  bundle: true,
+  logLevel: 'info',
+  outdir: 'dist/'
+})
+
+await dev.watch()
