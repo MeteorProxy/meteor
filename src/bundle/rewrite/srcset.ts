72acb9678959df9d64e5aeb73e99087d70fa9f79
proudparrot2 <knowing.discos0e@icloud.com>
2024-07-09 00:36:15 -0500

    other srcset

--- src/bundle/rewrite/srcset.ts
+++ src/bundle/rewrite/srcset.ts
@@ -1,13 +1,13 @@
 export function rewriteSrcset(srcset: string, origin: URL) {
-  const urls = srcset.split(/ [0-9]+x,? ?/g)
-  if (!urls) return ''
-  const sufixes = srcset.match(/ [0-9]+x,? ?/g)
-  if (!sufixes) return ''
-  const rewrittenUrls = urls.map((url, i) => {
-    if (url && sufixes[i]) {
-      return self.Meteor.rewrite.url.encode(url, origin) + sufixes[i]
-    }
-  })
-
-  return rewrittenUrls.join('')
+  return srcset
+    .split(', ')
+    .map((set) => {
+      return set
+        .split(' ')
+        .map((url, index) => {
+          return index === 0 ? self.Meteor.rewrite.url.encode(url, origin) : url
+        })
+        .join(' ')
+    })
+    .join(', ')
 }
