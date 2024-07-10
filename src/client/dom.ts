7f27e4b1e181c651e5e0aec0ec12e1a6ffa3494f
proudparrot2 <knowing.discos0e@icloud.com>
2024-07-10 00:15:02 -0500

    Watch more dom attributes

--- src/client/dom.ts
+++ src/client/dom.ts
@@ -1,10 +1,15 @@
-const attribs: Record<string, (new (...args: unknown[]) => HTMLElement)[]> = {
-  src: [HTMLScriptElement, HTMLVideoElement, HTMLImageElement],
-  href: [HTMLAnchorElement, HTMLLinkElement]
-}
+const attributes: Record<string, (new (...args: unknown[]) => HTMLElement)[]> =
+  {
+    src: [HTMLScriptElement, HTMLVideoElement, HTMLImageElement],
+    href: [HTMLAnchorElement, HTMLLinkElement],
+    style: [HTMLStyleElement],
+    srcdoc: [HTMLIFrameElement],
+    srcset: [HTMLImageElement],
+    action: [HTMLFormElement],
+    formaction: [HTMLInputElement]
+  }
 
-
-for (const [attr, elms] of Object.entries(attribs)) {
+for (const [attr, elms] of Object.entries(attributes)) {
   for (const elm of elms) {
     const descriptors = Object.getOwnPropertyDescriptor(elm, attr)
 
